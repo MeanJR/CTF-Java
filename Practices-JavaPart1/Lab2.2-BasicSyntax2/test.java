f83a12d6051f405eef13622b5be56c22ae047fe9
MeanJR <t.yongmint.jr@gmail.com>
2021-04-26 06:45:50 +0700

    Test2

---
+++ Practices-JavaPart1/Lab2.2-BasicSyntax2/test.java
@@ -0,0 +1,35 @@
+package com.ctf.BasicSyntax1;
+
+public class BasicSyntax1 {
+	// 1
+	// This is a single line comment.
+
+	/*
+	 * This is a multi-line comment.
+	 * 
+	 * Hello Eclipse IDE!!
+	 * 
+	 * Let's start Lab 1
+	 * 
+	 */
+
+	public static void main(String[] args) {
+
+		// 2
+
+		int number = 0;
+		boolean flag = true;
+		String name = "Mark";
+		double price = 23.353d;
+		char aplabet = 'A';
+
+		// 3
+
+		System.out.println("This is a Integer " + number);
+		System.out.println("This is a boolean " + flag);
+		System.out.println("This is a String " + name);
+		System.out.println("This is a double " + price);
+		System.out.println("This is a character " + aplabet);
+	}
+
+}
